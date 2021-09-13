1b02e93b3d69397e167fc17d00e8381e205ea58f
aidanlee09 <aidanlee09@gmail.com>
2021-09-13 00:15:03 -0700

    Update singlePendulum.py

--- singlePendulum.py
+++ singlePendulum.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 import matplotlib.animation as animation
 from collections import deque
-import matplotlib.patches as mp
+import matplotlib.patches as p
 
 G = 9.8  # acceleration due to gravity, in m/s^2
 L1 = 1.0  # length of pendulum 1 in m
 L2 = 1.0  # length of pendulum 2 in m
 L = L1 + L2  # maximal length of the combined pendulum
-M1 = 1.0  # mass of pendulum 1 in kg
-M2 = 10000  # mass of pendulum 2 in kg
+mc = 5.0  # mass of cart
+mp = 1.0  # mass of pendulum
 t_stop = 20  # how many seconds to simulate
 history_len = 500  # how many trajectory points to display
 
@@ -49,10 +49,10 @@ def derivs(state, t):
     x, v, theta, omega = state
     dstatedt = np.zeros(4)
     dstatedt[0] = v
-    F = 0
-    dstatedt[1] = (F + M2*np.sin(theta)*(L*omega**2 + G*np.cos(theta)))/(M1+M2*np.sin(theta)**2)
+    F = 1000*(np.pi - theta) - 100*omega - 10*x
+    dstatedt[1] = (F + mp*np.sin(theta)*(L*omega**2 + G*np.cos(theta)))/(mc+mp*(np.sin(theta))**2)
     dstatedt[2] = omega
-    dstatedt[3] = (-1*F*np.cos(theta) - M2*L*(omega**2)*np.cos(theta)*np.sin(theta) - (M1 + M2)*G*np.sin(theta))/(L*(M1+M2*np.sin(theta)**2))
+    dstatedt[3] = (-1*F*np.cos(theta) - mp*L*(omega**2)*np.cos(theta)*np.sin(theta) - (mc + mp)*G*np.sin(theta))/(L*(mc + mp*(np.sin(theta))**2))
 
     return dstatedt
 
@@ -71,7 +71,7 @@ t = np.arange(0, t_stop, dt)
 # =============================================================================
 x = 0
 v = 0
-theta = np.pi/2
+theta = 3*np.pi/4
 omega = 0
 
 #initial state
@@ -104,7 +104,7 @@ ax.grid()
 cartpos_list = y[:,0]
 line, = ax.plot([], [], 'o-', lw=2)
 trace, = ax.plot([], [], ',-', lw=1)
-rectangle = mp.Rectangle((0, 0), 0, 0, color = "orange", lw = 2)
+rectangle = p.Rectangle((0, 0), 0, 0, color = "orange", lw = 2)
 plt.gca().add_patch(rectangle)
 time_template = 'time = %.1fs'
 time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
@@ -125,8 +125,8 @@ theta_list = y[:,2]
 
 def animate(i):
     
-    thisx = [cartpos_list[i], L*cos(theta_list[i])+cartpos_list[i]]
-    thisy = [0, L*sin(theta_list[i])]
+    thisx = [cartpos_list[i], L*cos(theta_list[i]-np.pi/2)+cartpos_list[i]]
+    thisy = [0, L*sin(theta_list[i]-np.pi/2)]
 
     if i == 0:
         history_x.clear()
